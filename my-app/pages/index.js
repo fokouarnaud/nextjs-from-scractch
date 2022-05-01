b4153d1f80e60e2f2b7b03b800bdeb30ad5db5b6
fokouarnaud <fokouarnaud@gmail.com>
2022-05-01 21:57:03 +0100

    update

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,5 +1,24 @@
- function HomePage(){
-   return <h1>Home page</h1>;
- }
+import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Cathedral_of_Albi_-_Nave_and_Organ_-_7029.jpg',
+    adress: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup'
+  },{
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Cathedral_of_Albi_-_Nave_and_Organ_-_7029.jpg',
+    adress: 'Some address 6, 125345 Some City',
+    description: 'This is a second meetup'
+  },
 
- export default HomePage;
+]
+
+function HomePage() {
+  return <Layout> <MeetupList meetups={DUMMY_MEETUPS } /></Layout>
+}
+
+export default HomePage;
---
+++ my-app/pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import Layout from "../../components/layout/Layout";
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData){
+        console.log(enteredMeetupData);
+    }
+  return <Layout><NewMeetupForm onAddMeetup={addMeetupHandler } /></Layout>
+}
+
+export default NewMeetupPage;
